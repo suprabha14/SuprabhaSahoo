66d233f866311b2f2393c092a0d50bf88fc419d7
suprabha14 <suprabhasahoo2002@gmail.com>
2024-08-07 01:58:14 +0530

    Exercise 1-8

---
+++ Week2_Spring_Core_Maven/librarymanagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Other service methods
+}
