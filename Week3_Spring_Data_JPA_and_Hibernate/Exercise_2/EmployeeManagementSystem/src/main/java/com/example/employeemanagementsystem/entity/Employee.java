ffbb92107b184185162d27dc1c55e858a0f9533e
suprabha14 <suprabhasahoo2002@gmail.com>
2024-08-10 01:29:10 +0530

    Exercise 2

---
+++ Week3_Spring_Data_JPA_and_Hibernate/Exercise_2/EmployeeManagementSystem/src/main/java/com/example/employeemanagementsystem/entity/Employee.java
@@ -0,0 +1,14 @@
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+}
