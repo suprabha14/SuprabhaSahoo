27aeb3e885c79ae9e391ec40c4c521bd316e755c
suprabha14 <suprabhasahoo2002@gmail.com>
2024-08-10 02:04:17 +0530

    Exercise 3

---
+++ Week3_Spring_Data_JPA_and_Hibernate/Exercise_3/EmployeeManagementSystem/src/main/java/com/example/employeemanagementsystem/entity/Department.java
@@ -0,0 +1,12 @@
+@Entity
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+    private List<Employee> employees = new ArrayList<>();
+
+}
